12bcf3fe1cdda33439be8971b2388fa9c248aaab
xuekaikai <2780739366@qq.com>
2023-09-03 22:15:53 +0800

    react脚手架

---
+++ react_staging/02_src_HelloReact/index.js
@@ -0,0 +1,9 @@
+// 引入react核心库
+import React from "react";
+// 引入reactDOM
+import ReactDOM from "react-dom";
+// 引入app
+import App from "./App.jsx"
+
+// 渲染app到页面
+ReactDOM.render(<App/>, document.getElementById('root'))
